4170759b33950dcb03d80a3fa49036d4f86472f4
AVAXDEX <96629427+AvaxDex@users.noreply.github.com>
2022-06-29 16:52:47 +0700

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -17,7 +17,7 @@ const Navbar = ({ accounts, setAccounts }) => {
                         <div className="items items-center mx-20 justify-between w-full text-center sm:flex lg:flex">
                             <div className="flex justify-center items-center flex-shrink-0">
                                 <h1 className="font-justanotherhand text-5xl cursor-pointer text-maastricht">
-                                Adex<span className="text-pale-yellow">Story</span>
+                                AdexStory<span className="text-pale-yellow">NFTs</span>
                                 </h1>
                             </div>
                             {/* Connect */}
