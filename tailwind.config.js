6c2a8fff54a9b95b6aaf7aa039bbd50453d23822
AVAXDEX <96629427+AvaxDex@users.noreply.github.com>
2022-06-29 10:07:55 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
   ],
   theme: {
     colors: {
-      'teal': 'rgb(131,58,180);
-background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 0%, rgba(252,176,69,1) 100%)',
+      'teal': '#fd1d1d',
       'pale-yellow': '#fff8c2',
       'maastricht': '#062231',
     },
