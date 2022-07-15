3b70c3fb302f733a107e60b0db56693d2a52c106
AVAXDEX <96629427+AvaxDex@users.noreply.github.com>
2022-07-15 18:29:12 +0700

    Update team.js

--- components/team.js
+++ components/team.js
@@ -2,6 +2,12 @@ import Image from 'next/image'
 
 const people = [
   {
+    name: 'NFT Calendar',
+    url: 'https://nftcalendar.io/event/kid-story',
+    role: '',
+    imageSrc:'/images/team/profile-1.png',
+  },
+  {
     name: 'Sopha',
     role: 'Marketing Manager',
     imageSrc:'/images/team/profile-1.png',
